93d345a867e4ac7d53367f7cef26dccdb01c4cd8
Siddharth Ravikumar <ravikumar.siddharth@gmail.com>
2023-04-09 16:52:39 -0400

    Ably sdk integration causing build failure

--- Sources/openLiveAvatarSDK/openLiveAvatarSDK.swift
+++ Sources/openLiveAvatarSDK/openLiveAvatarSDK.swift
@@ -1,5 +1,9 @@
+import Ably
+
 public struct openLiveAvatarSDK {
     public private(set) var text = "Hello, World!"
+    let API_KEY = ""
+    let client = ARTRealtime(key: API_KEY)
 
     public init() {
     }
