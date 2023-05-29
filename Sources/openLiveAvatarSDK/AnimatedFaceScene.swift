7e3fb66986ce6c9e4ea54288e3de8d5a886a0a89
yifuyin <yifuyin7@gmail.com>
2023-05-28 23:46:54 -0700

    finish seperating out Avatar with its own wrapper.

--- Sources/openLiveAvatarSDK/AnimatedFaceScene.swift
+++ Sources/openLiveAvatarSDK/AnimatedFaceScene.swift
@@ -4,22 +4,27 @@ import ARKit
 public class AnimatedFaceScene: NSObject {
     // Set up face component nodes
     public var animatedFaceScene = SKScene();
-    let leftEye = SKSpriteNode(imageNamed: "leftEye")
-    let rightEye = SKSpriteNode(imageNamed: "rightEye")
-    let mouth = SKSpriteNode(imageNamed: "mouth")
+    var leftEye = SKSpriteNode(imageNamed: "leftEye")
+    var rightEye = SKSpriteNode(imageNamed: "rightEye")
+    var mouth = SKSpriteNode(imageNamed: "mouth")
+    
+    public init(leftEyeImage : String, rightEyeImage : String, mouthImage : String) {
+        super.init()
+        leftEye = SKSpriteNode(imageNamed: leftEyeImage)
+        rightEye = SKSpriteNode(imageNamed: rightEyeImage)
+        mouth = SKSpriteNode(imageNamed: mouthImage)
+    }
     
     let leftEyeScale = 0.05
     let rightEyeScale = 0.05
     let mouthScale = 0.2
     // Add other face components as needed
     
-    public func moveIt(view : UIView) {
-        animatedFaceScene = SKScene(size: view.bounds.size)
+    public func moveIt(bound : CGSize) {
+        animatedFaceScene = SKScene(size: bound)
         leftEye.position = CGPoint(x: 300, y: 600)
         rightEye.position = CGPoint(x: 100, y: 600)
         mouth.position = CGPoint(x: 200, y: 400)
-        // Set up other face components
-        print("test1")
         
         leftEye.setScale(leftEyeScale)
         rightEye.setScale(rightEyeScale)
@@ -30,32 +35,30 @@ public class AnimatedFaceScene: NSObject {
     }
 
     // Update face component animations based on blend shape coefficients
-    func updateFaceComponents(_ blendShapes: [ARFaceAnchor.BlendShapeLocation: NSNumber]) {
+    func updateFaceComponents(avatarState : AvatarState) {
         // Example: Update eye scale based on eye blink blend shape coefficients
-        if let leftEyeBlink = blendShapes[.eyeBlinkLeft]?.floatValue {
-            leftEye.yScale = leftEyeScale - Double(leftEyeBlink) * leftEyeScale
-        }
+        let leftEyeBlink = avatarState.eyeBlinkLeft   // {
+        leftEye.yScale = leftEyeScale - Double(leftEyeBlink) * leftEyeScale
+        //}
         
-        if let rightEyeBlink = blendShapes[.eyeBlinkRight]?.floatValue {
-            rightEye.yScale = leftEyeScale - Double(rightEyeBlink) * rightEyeScale
-        }
+        let rightEyeBlink = avatarState.eyeBlinkRight // {
+        rightEye.yScale = rightEyeScale - Double(rightEyeBlink) * rightEyeScale
+        //}
         
-        if let mouthFunnel = blendShapes[.mouthFunnel]?.floatValue {
-            mouth.xScale = mouthScale - Double(mouthFunnel) * mouthScale
-        }
+        let mouthFunnel = avatarState.mouthFunnel     // {
+        mouth.xScale = mouthScale - Double(mouthFunnel) * mouthScale
+        //}
         
-        if let jawOpen = blendShapes[.jawOpen]?.floatValue {
-            print(jawOpen)
-            mouth.yScale = Double(jawOpen) * mouthScale
-        }
-        
+        let jawOpen = avatarState.jawOpen             // {
+        print(jawOpen)
+        mouth.yScale = Double(jawOpen) * mouthScale
+//        }
 //        if let cheekPuff = blendShapes[.cheekPuff]?.floatValue {
 //            print(cheekPuff)
 //        }
-        
 //        if let jawOpen = blendShapes[.jawOpen]?.floatValue {
 //            rightEye.yScale = leftEyeScale - Double(jawOpen) * rightEyeScale
 //        }
-        // Update other face components based on blend shape coefficients
+//        Update other face components based on blend shape coefficients
     }
 }
--- Sources/openLiveAvatarSDK/LiveAvatarController.swift
+++ Sources/openLiveAvatarSDK/LiveAvatarController.swift
@@ -3,56 +3,42 @@ import ARKit
 
 public class LiveAvatarController: NSObject {
     private let synchronizer: AvatarStateSynchronizer
-    private var avatars: [String: Avatar] = [:]
-    public var frontARSCNView = ARSCNView()
+    private var avatars: [String: AnimatedFaceSceneWrapper] = [:]
     private var timeofcurrent = Date().timeIntervalSince1970
-    public var skView: SKView
-    var faceScene: AnimatedFaceScene
     
-    public init(apiKey: String, channelName: String, view : UIView) {
-        print("Information Information")
+    public init(apiKey: String, channelName: String) {
         self.synchronizer = AvatarStateSynchronizer(apiKey: apiKey, channelName: channelName)
-        self.skView = SKView(frame: view.frame)
-        faceScene = AnimatedFaceScene()
-        faceScene.moveIt(view: view)
-        skView.ignoresSiblingOrder = true
-        self.skView.presentScene(faceScene.animatedFaceScene)
-        super.init()
-    }
-    
-    public func startCapture() {
-        guard ARFaceTrackingConfiguration.isSupported else {
-            print("Face tracking not supported on this device")
-            return
-        }
-        print("Face tracking is supported")
-
-        frontARSCNView.delegate = self
-        frontARSCNView.session.delegate = self
         
-        setupARFaceTracking()
+        super.init()
         
         synchronizer.subscribeToStateUpdates(event:  "avatar-state-update") { [weak self] (result: Result<AvatarState, Error>) in
             switch result {
             case .success(let avatarState):
                 print("success")
-                self?.faceScene.updateFaceComponents(self?.getBlendShapes(avatarState) ?? [ARFaceAnchor.BlendShapeLocation: NSNumber]())
+                self?.updateAvatars(with: avatarState)
+                
             case .failure(let error):
                 print("Failed to receive avatar state update:", error)
             }
         }
     }
     
-    public func getBlendShapes(_ avatarState: AvatarState) -> [ARFaceAnchor.BlendShapeLocation: NSNumber]{
-        var blendShapes: [ARFaceAnchor.BlendShapeLocation: NSNumber] = [ARFaceAnchor.BlendShapeLocation: NSNumber]()
-        blendShapes[.eyeBlinkLeft] = NSNumber(value: avatarState.eyeBlinkLeft)
-        blendShapes[.eyeBlinkRight] = NSNumber(value: avatarState.eyeBlinkRight)
-        blendShapes[.mouthFunnel] = NSNumber(value: avatarState.mouthFunnel)
-        blendShapes[.jawOpen] = NSNumber(value: avatarState.jawOpen)
-        return blendShapes
+    public func addWrapperToUIVIew(view:UIView) {
+        let wrapper = AnimatedFaceSceneWrapper(frame: view.frame, emittingFunc: self.emittingFromFacesceneWrapperCallback)
+        wrapper.startCapture()
+        self.addAvatar(id: "init", avatar: wrapper)
+        wrapper.addToUIWindow(view: view)
     }
     
-    public func addAvatar(id: String, avatar: Avatar) {
+    public func emittingFromFacesceneWrapperCallback(avatarState : AvatarState){
+        do {
+            try synchronizer.publishStateUpdate(event: "avatar-state-update", data: avatarState)
+        } catch {
+            print(error)
+        }
+    }
+    
+    public func addAvatar(id: String, avatar: AnimatedFaceSceneWrapper) {
         avatars[id] = avatar
     }
 
@@ -62,83 +48,7 @@ public class LiveAvatarController: NSObject {
 
     private func updateAvatars(with state: AvatarState) {
         for (_, avatar) in avatars {
-            avatar.update(with: state)
-        }
-    }
-}
-
-extension LiveAvatarController: ARSessionDelegate {
-    /// ARFaceTrackingSetup
-    func setupARFaceTracking() {
-        // check if the device supports ARFaceTracking
-        guard ARFaceTrackingConfiguration.isSupported else { return }
-        print("device supports ARFaceTracking")
-        let configuration = ARFaceTrackingConfiguration()
-        // When you enable the `isLightEstimationEnabled` setting, a face-tracking configuration estimates directional and environmental lighting (an `ARDirectionalLightEstimate` object) by referring to the detected face as a light probe. [no need for now]
-        // true: CPU 51% Memory 164MB
-        // false: CPU 45% Memory 178MB
-        configuration.isLightEstimationEnabled = false
-        if #available(iOS 13.0, *) {
-            configuration.maximumNumberOfTrackedFaces = 1
-            print("Turning maximumNumberOfTrackedFaces to 1")
-        } else {
-            print("Turning maximumNumberOfTrackedFaces to 0")
-        } // default value is one
-        
-        frontARSCNView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
-    }
-
-    public func session(_: ARSession, didFailWithError error: Error) {
-        print("The AR session started.")
-        
-        guard error is ARError else { return }
-
-        let errorWithInfo = error as NSError
-        let messages = [
-            errorWithInfo.localizedDescription,
-            errorWithInfo.localizedFailureReason,
-            errorWithInfo.localizedRecoverySuggestion,
-        ]
-        let errorMessage = messages.compactMap { $0 }.joined(separator: "\n")
-
-        DispatchQueue.main.async {
-            print("The AR session failed. ::" + errorMessage)
-        }
-    }
-
-    public func sessionWasInterrupted(_: ARSession) {
-        print("The AR session sessionWasInterrupted.")
-    }
-
-    public func sessionInterruptionEnded(_: ARSession) {
-        print("The AR session sessionInterruptionEnded.")
-        DispatchQueue.main.async {
-            self.setupARFaceTracking()
-        }
-    }
-}
-
-extension LiveAvatarController: ARSCNViewDelegate {
-    // MARK: - Properties
-    // MARK: - ARSCNViewDelegate
-    /// - Tag: ARNodeTracking
-    public func renderer(_: SCNSceneRenderer, didAdd _: SCNNode, for _: ARAnchor) {
-    }
-
-    /// - Tag: ARFaceGeometryUpdate
-    public func renderer(_: SCNSceneRenderer, didUpdate _: SCNNode, for anchor: ARAnchor) {
-        guard let faceAnchor = anchor as? ARFaceAnchor else { return }
-        guard let eyeBlinkLeft = faceAnchor.blendShapes[.eyeBlinkLeft] as? Float,
-              let eyeBlinkRight = faceAnchor.blendShapes[.eyeBlinkRight] as? Float,
-              let mouthFunnel = faceAnchor.blendShapes[.mouthFunnel] as? Float,
-              let jawOpen = faceAnchor.blendShapes[.jawOpen] as? Float
-        else { return }
-        
-        let avatarState = AvatarState(eyeBlinkRight: eyeBlinkRight, eyeBlinkLeft: eyeBlinkLeft, mouthFunnel: mouthFunnel, jawOpen: jawOpen)
-        do {
-            try synchronizer.publishStateUpdate(event: "avatar-state-update", data: avatarState)
-        } catch {
-            print(error)
+            avatar.update(avatarState: state)
         }
     }
 }
